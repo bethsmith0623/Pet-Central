609f07ced082f046fceaa7a5916b94daebb87bc2
Beth Smith <bethsmith0623@gamil.com>
2019-08-23 15:05:35 -0500

    adds routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pet Central' });
+  res.render('index', { user: req.user, title: 'Pet Central' });
 });
+
+//Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+//Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/users/index',
+    failureRedirect : '/index'
+  }
+));
+
+//OAuth logout route
+router.get('/logout', function(req,res) {
+  req.logout();
+  res.redirect('/');
+})
 
 module.exports = router;
--- routes/pets.js
+++ routes/pets.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('/pets/index', { title: 'Pet Central' });
+  res.send('./pets/index', { title: 'Pet Central' });
 });
 
 module.exports = router;
