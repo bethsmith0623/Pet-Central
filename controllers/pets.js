da2abd5cc8a15060bdf06c4d9373ba4767c33fe8
Beth Smith <bethsmith0623@gamil.com>
2019-08-25 17:59:14 -0500

    adds pet functions

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,5 +1,36 @@
-const Pet = require('../models/pet');
+var Pet = require('../models/pet');
 
 module.exports = {
-
+  index,
+  new: newPet,
+  create,
+  show
 };
+
+function index(req, res) {
+  Pet.find({}, function(err, pets) {
+    res.render('pets/index', {pets});
+  });
+};
+function show(req, res) {
+  Pet.findById (req.params.id, function(err, pet) {
+    console.log(pet);
+    res.render('pets/show');
+  });
+}
+
+function newPet(req, res) {
+  res.render('pets/new');
+};
+
+function create(req, res) {
+  // for (let key in req.body) {
+  //   if (req.body[key] === '') delete req.body[key];
+  // }
+  var pet = new Pet(req.body);
+  pet.save(function(err) {
+    if (err) return res.redirect('/pets/new');
+    console.log(pet);
+    res.redirect(`/pets/${pet._id}`);
+  })
+}
--- routes/pets.js
+++ routes/pets.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var petsCtrl = require('../controllers/pets');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('./pets/index', { title: 'Pet Central' });
+  res.send('./pets/index');
 });
+
+router.get('/', petsCtrl.index);
+router.get('/new', petsCtrl.new);
+router.get('/:id', petsCtrl.show);
+router.post('/', petsCtrl.create);
 
 module.exports = router;
